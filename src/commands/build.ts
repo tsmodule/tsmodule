96afac294107b75c347a1f15bfd182d9f0d4a3d9
C. Lewis <ctj.lewis@icloud.com>
2022-01-14 13:12:23 -0600

    chore: comments

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,11 +1,10 @@
+import { Plugin as RollupPlugin, rollup } from "rollup";
 import { dirname, extname, isAbsolute, relative, resolve as resolvePath } from "path";
 import { build as esbuild, BuildOptions } from "esbuild";
 import { readFile, rm } from "fs/promises";
 import chalk from "chalk";
 import glob from "fast-glob";
 import { pathToFileURL } from "url";
-
-import { Plugin as RollupPlugin, rollup } from "rollup";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore No types available for this plugin.
 import shebang from "rollup-plugin-preserve-shebang";
@@ -18,10 +17,16 @@ import shebang from "rollup-plugin-preserve-shebang";
 import { debugLog } from "../utils/log.js";
 import { resolve } from "../loader/index.js";
 
+/**
+ * Remove `file://` prefix from a path if it exists.
+ */
 const forceUnixPath = (path: string) => {
   return path.replace("file://", "");
 };
 
+/**
+ * Get a Unix-like relative path from a URL.
+ */
 const getRelativePath = (baseUrl: string, path: string) => {
   const relativePath = relative(baseUrl, forceUnixPath(path));
   return (
@@ -49,6 +54,9 @@ export const importPattern = (importSource: string) => {
   );
 };
 
+/**
+ * Rewrite an import/export/require statement.
+ */
 export const rewriteImport = (
   importStatement: string,
   importToReplace: string,
@@ -63,18 +71,20 @@ export const rewriteImport = (
   return importStatement.replace(sourcePart, rewrittenSource);
 };
 
+/**
+ * A Rollup plugin which rewrites all the imports in a chunk with the specifiers
+ * resolved by the TSM loader.
+ */
 export const rewriteImports: () => RollupPlugin = () => {
   return {
     name: "Rewrite imports",
     renderChunk: async (code: string, chunk, options) => {
       for (const importedChunk of chunk.imports) {
-        // debugLog({ importedChunk });
         /**
          * If no absolute module ID, bail.
          */
         const input = chunk.facadeModuleId;
         if (!input) continue;
-
         /**
          * Just a named module. Skip.
          */
@@ -97,10 +107,12 @@ export const rewriteImports: () => RollupPlugin = () => {
            * other incomplete specifier that must be resolved.
            */
         }
-
+        /**
+         * The base directory from which all relative imports in this entry
+         * are resolved.
+         */
         const baseDir = dirname(input);
         let importToReplace = importedChunk;
-
         /**
          * Rewrite remaining absolute specifiers relative to baseDir for
          * replacement.
@@ -109,20 +121,22 @@ export const rewriteImports: () => RollupPlugin = () => {
           debugLog(`Rewriting partial specifier: ${importedChunk}`);
           importToReplace = getRelativePath(baseDir, importedChunk);
         }
-
         debugLog({ importToReplace });
-
         /**
          * Read the matched import/require statements and replace them.
          */
         const importMatch = importPattern(importToReplace);
         const importStatements = code.match(importMatch) ?? [];
-        // debugLog({ importStatements });
-
-        // debugLog({ file: options.file, imports: chunk.imports, importStatements });
+        /**
+         * Attempt to replace the specifier for each import statement.
+         */
         for (const importStatement of importStatements) {
           if (options.file) {
             const parentURL = pathToFileURL(resolvePath(input)).href;
+            /**
+             * Resolve the specifier to a module and URL using the internal
+             * loader.
+             */
             const resolvedImport = await resolve(
               importedChunk,
               {
@@ -133,45 +147,52 @@ export const rewriteImports: () => RollupPlugin = () => {
             );
             debugLog({ resolvedImport });
             /**
-             * Rewrite import identifiers for seamless CJS support. Ignore dynamic
-             * imports.
+             * Rewrite import identifiers for seamless CJS support. Ignore
+             * dynamic imports.
              */
             if (resolvedImport.url) {
               const unixLikePath = resolvedImport.url.replace("file://", "");
               debugLog({ importedChunk, parentURL, unixLikePath });
+              /**
+               * The import statement with the unresolved import replaced with
+               * its resolved specifier.
+               */
               const rewrittenImport = rewriteImport(
                 importStatement,
                 importToReplace,
                 getRelativePath(baseDir, unixLikePath),
               );
-
+              /**
+               * Replace the import in the code.
+               */
               debugLog({ input, importStatement, rewrittenImport });
               code = code.replace(importStatement, rewrittenImport);
             }
-            // debugLog(parentURL);
-            // debugLog(rewrittenImport);
           }
         }
       }
       
-      // return null;
       return {
         code,
         sourcemap: false,
       };
-      // for (const chunkImport of chunk.imports) {
-      //   debugLog({ chunkImport });
-      // }
     },
   };
 };
 
+/**
+ * Build TS to JS. This will contain incomplete specifiers like `./foo` which
+ * could mean many things, all of which is handled by the loader which will
+ * resolve them for us.
+ */
 export const build = async (production = false) => {
   try {
     if (production) {
       debugLog(chalk.grey("Building for production..."));
     }
-
+    /**
+     * Initialize build options, and inject PACKAGE_JSON for library builds.
+     */
     const cwd = process.cwd();
     const pkgJsonFile = resolvePath(cwd, "package.json");
     const pkgJson = await readFile(pkgJsonFile, "utf-8");
@@ -186,16 +207,22 @@ export const build = async (production = false) => {
         PACKAGE_JSON: pkgJson,
       },
     };
-
+    /**
+     * Clean old output.
+     */
     const distDir = resolvePath(cwd, "dist");
     await rm(distDir, { force: true, recursive: true });
-          
+    /**
+     * TS files to compile.
+     */
     const tsFiles = await glob("src/**/*.ts", { cwd });
     await esbuild({
       ...shared,
       entryPoints: tsFiles.filter((file) => !file.endsWith(".d.ts")),
     });
-
+    /**
+     * TSX files to compile.
+     */
     const tsxFiles = await glob("src/**/*.tsx", { cwd });
     await esbuild({
       ...shared,
@@ -205,7 +232,9 @@ export const build = async (production = false) => {
         js: "import {jsx as _jsx} from 'react/jsx-runtime.js';\n",
       },
     });
-
+    /**
+     * Run the post-build process and resolve import specifiers in output.
+     */
     await postBuild();
   } catch (err) {
     console.error(err);
@@ -213,6 +242,9 @@ export const build = async (production = false) => {
   }
 };
 
+/**
+ * Finally, rewrite imports in the emitted JS to ESM-compliant paths.
+ */
 export const postBuild = async () => {
   const filesToOptimize = await glob("dist/**/*.js");
   await Promise.all(
@@ -221,14 +253,32 @@ export const postBuild = async () => {
         debugLog("Optimizing", file);
         const build = await rollup({
           input: file,
+          /**
+           * Mark all imports other than this entry point as external (do not
+           * bundle).
+           */
           external: (id: string) => id !== file,
-          plugins: [rewriteImports(), shebang()],
+          plugins: [
+            /**
+             * Leave #!/usr/bin/env shebangs.
+             */
+            shebang(),
+            /**
+             * Rewrite import specifiers using the internal loader.
+             */
+            rewriteImports()
+          ],
+          /**
+           * Suppress warnings about empty modules.
+           */
           onwarn: () => void 0,
         });
-
+        /**
+         * Write the spec-compliant ES module output.
+         */
         await build.write({
           file,
-          format: "es",
+          format: "esm",
         });
       }
     )
