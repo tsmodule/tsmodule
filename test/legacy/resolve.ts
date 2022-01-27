39c70c00f2229580269ef147fc45cd31b6a1ba81
C. Lewis <ctj.lewis@icloud.com>
2022-01-27 15:00:20 -0600

    chore: update resolution test

--- test/legacy/resolve.ts
+++ test/legacy/resolve.ts
@@ -1,13 +1,16 @@
 #!/usr/bin/env tsm
-
-import { resolve } from "../../src/loader";
 import test from "ava";
 
+import { pathToFileURL } from "url";
+import { resolve } from "../../src/loader";
+import { resolve as resolvePath } from "path";
+
 test("should resolve to TS files for JS imports in loader mode", async (t) => {
+  const testEntry = resolvePath("./src/loader/index.ts");
   const { url } = await resolve(
-    "../../src/commands/build.js",
+    "../commands/build.js",
     {
-      parentURL: "file:///home/christian/PersonalProjects/tsm/src/loader/index.ts",
+      parentURL: pathToFileURL(testEntry).href,
       conditions: [ "node", "import", "node-addons" ]
     },
     async (url) => await import(url),
