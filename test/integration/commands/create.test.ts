1e1c115ef368ac1bc01822dce1a5f71f5d1fdee5
C. Lewis <ctj.lewis@icloud.com>
2022-02-01 18:04:35 -0600

    chore: debug CI

--- test/integration/commands/create.test.ts
+++ test/integration/commands/create.test.ts
@@ -5,6 +5,7 @@ import { shell } from "await-shell";
 import { tmpdir } from "os";
 
 process.chdir(tmpdir());
+await shell("yarn link @tsmodule/tsm");
 
 test.serial("`create` should generate TS module package", async (t) => {
   t.timeout(20000);
