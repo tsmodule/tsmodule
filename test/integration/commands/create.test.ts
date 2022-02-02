6f5693ab3a3d6d89be692071ed58a4595d600d47
C. Lewis <ctj.lewis@icloud.com>
2022-02-02 14:23:15 -0600

    chore: use shell `cd` over `process.chdir` to change directory, prevent ava issues
    see: https://github.com/avajs/ava/issues/2945

--- test/integration/commands/create.test.ts
+++ test/integration/commands/create.test.ts
@@ -1,29 +1,30 @@
 import test from "ava";
 
 import { resolve } from "path";
+import { rm } from "fs/promises";
 import { shell } from "await-shell";
 import { tmpdir } from "os";
 
-process.chdir(tmpdir());
+const testModuleDir = resolve(tmpdir(), "test-module");
+
 await shell("yarn link @tsmodule/tsmodule");
+await rm(testModuleDir, { recursive: true, force: true });
 
 test.serial("`create` should generate TS module package", async (t) => {
   t.timeout(120_000);
 
-  await shell("tsmodule create test-module");
+  await shell(`cd ${tmpdir()} && tsmodule create test-module`);
 
   /**
    * `tsmodule create` adds a `@tsmodule/tsmodule` dependency, so re-link it in
    * the test module.
    */
-  await shell("yarn --cwd test-module link @tsmodule/tsmodule");
   t.pass();
 });
 
 test.serial("created module package should build", async (t) => {
   t.timeout(120_000);
-  process.chdir(resolve(tmpdir(), "test-module"));
 
-  await shell("tsmodule build");
+  await shell(`cd ${testModuleDir} && tsmodule build`);
   t.pass();
 });
