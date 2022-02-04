0d3ce106654421abffb82df7851026dff72a3f82
C. Lewis <ctj.lewis@icloud.com>
2022-02-03 18:22:50 -0600

    chore: refactor tests

--- test/integration/commands.test.ts
+++ test/integration/commands.test.ts
@@ -12,18 +12,31 @@ await fs.rm(testModuleDir, { recursive: true, force: true });
 
 test.serial("[create] should generate TS module package", async (t) => {
   process.chdir(tmpdir());
+
+  /**
+   * Create the test TS module.
+   */
   await shell(`cd ${tmpdir()} && tsmodule create test-module`);
 
   /**
-   * `tsmodule create` adds a `@tsmodule/tsmodule` dependency, so re-link it in
-   * the test module.
+   * `tsmodule create` adds a `@tsmodule/tsmodule` dependency, so re-link it.
    */
+  await shell(`cd ${testModuleDir} && yarn link @tsmodule/tsmodule`);
+
   t.pass();
 });
 
 test.serial("[create] created module package should build", async (t) => {
   process.chdir(testModuleDir);
-  await shell(`cd ${testModuleDir} && tsmodule build && node dist/index.js`);
+  await shell(`cd ${testModuleDir} && tsmodule build`);
+
+  t.pass();
+});
+
+test.serial("[create] built module should execute", async (t) => {
+  process.chdir(testModuleDir);
+  await shell(`cd ${testModuleDir} && node dist/index.js`);
+
   t.pass();
 });
 
