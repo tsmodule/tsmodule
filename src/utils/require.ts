b9a1b0e36480dafaeaecad3758f13464d7a12a9a
C. Lewis <ctj.lewis@icloud.com>
2022-03-27 15:02:28 -0500

    fix: only run require shim in non-DOM contexts

--- src/utils/require.ts
+++ src/utils/require.ts
@@ -1,23 +1,26 @@
-await (async () => {
-  const { dirname } = await import("path");
-  const { fileURLToPath } = await import("url");
+// @ts-ignore - Don't type-check `document`, runs in browser.
+if (typeof document === "undefined") {
+  await (async () => {
+    const { dirname } = await import("path");
+    const { fileURLToPath } = await import("url");
 
-  /**
-   * Shim entry-point related paths.
-   */
-  if (typeof globalThis.__filename === "undefined") {
-    globalThis.__filename = fileURLToPath(import.meta.url);
-  }
-  if (typeof globalThis.__dirname === "undefined") {
-    globalThis.__dirname = dirname(globalThis.__filename);
-  }
-  /**
-   * Shim require if needed.
-   */
-  if (typeof globalThis.require === "undefined") {
-    const { default: module } = await import("module");
-    globalThis.require = module.createRequire(import.meta.url);
-  }
-})();
+    /**
+     * Shim entry-point related paths.
+     */
+    if (typeof globalThis.__filename === "undefined") {
+      globalThis.__filename = fileURLToPath(import.meta.url);
+    }
+    if (typeof globalThis.__dirname === "undefined") {
+      globalThis.__dirname = dirname(globalThis.__filename);
+    }
+    /**
+     * Shim require if needed.
+     */
+    if (typeof globalThis.require === "undefined") {
+      const { default: module } = await import("module");
+      globalThis.require = module.createRequire(import.meta.url);
+    }
+  })();
+}
 
 export {};
