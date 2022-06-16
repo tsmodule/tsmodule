82f05bc6438e6d6cc77b0608adcd5e161d99005e
C. Lewis <ctj.lewis@icloud.com>
2022-06-15 20:51:45 -0500

    feat: add specification

--- src/specification/index.ts
+++ src/specification/index.ts
@@ -1,30 +1,132 @@
-import { TsmoduleSpecification } from "./types";
+import { ProjectConfiguration, TsmoduleSpecification } from "./types";
 
-export const specification: TsmoduleSpecification = {
-  default: {
-    dependencies: [],
-    devDependencies: [
-      "@types/node",
-      "@tsmodule/tsmodule",
-      "@typescript-eslint/eslint-plugin",
-      "@typescript-eslint/parser",
-      "ava",
-      "eslint",
-      "typescript",
-    ],
+/**
+ * Default configuration is always applied to a project first.
+ */
+const defaultSettings: ProjectConfiguration = {
+  packageJson: {
+    /**
+     * TS modules are ESM packages.
+     */
+    type: "module",
+    /**
+     * By default, they target Node.
+     */
+    platform: "node",
+    /**
+     * They emit types at the runtime index.
+     */
+    types: "dist/index.d.ts",
+    /**
+     * They package all files in dist/.
+     */
+    files: ["dist"],
+    /**
+     * And they emulate CJS-like submodule resolution.
+     *
+     * @example
+     * ```ts
+     * import { test } from  "my-package/a/b/c";
+     * // resolves to dist/a/b/c.index.js
+     * // source code at src/a/b/c/index.ts
+     * ```
+     */
+    exports: {
+      "./package.json": "./package.json",
+      ".": "./dist/index.js",
+      "./*": "./dist/*/index.js"
+    },
+    /**
+     * Apply build, dev, lint, test, and publish scripts.
+     */
+    scripts: {
+      "dev": "tsmodule dev",
+      "build": "tsmodule build",
+      "test": "ava",
+      "lint": "eslint src --fix",
+      "pretest": "tsmodule build -r",
+      "prepublishOnly": "yarn build && yarn test"
+    },
+    /**
+     * Set Ava config for testing.
+     */
+    ava: {
+      "timeout": 240000,
+      "files": [
+        "test/**/*.test.ts"
+      ],
+      "extensions": {
+        "ts": "module"
+      },
+      "nodeArguments": [
+        "--no-warnings",
+        "--loader=@tsmodule/tsmodule"
+      ]
+    },
   },
+  /**
+   * Standardize TSConfig and ESLint.
+   */
+  files: [
+    "tsconfig.json",
+    ".eslintrc",
+  ],
+  /**
+   * A Node program will not need any runtime deps by default.
+   */
+  dependencies: [],
+  /**
+   * It will need types, plus build/lint/test deps.
+   */
+  devDependencies: [
+    "@types/node",
+    "@tsmodule/tsmodule",
+    "@typescript-eslint/eslint-plugin",
+    "@typescript-eslint/parser",
+    "ava",
+    "eslint",
+    /**
+     * Ensure TSC is available for .d.ts generation.
+     */
+    "typescript",
+  ],
+};
+
+/**
+ * Standardized configuration settings for all tsmodule project types.
+ */
+export const specification: TsmoduleSpecification = {
+  default: defaultSettings,
   react: {
+    /**
+     * No additional package.json settings required.
+     */
+    packageJson: {},
+    /**
+     * Ensure Next, PostCSS, and Tailwind configs are available.
+     */
+    files: [
+      "next.config.js",
+      "postcss.config.js",
+      "tailwind.config.js",
+    ],
     dependencies: ["react@^17.0.2", "react-dom@^17.0.2"],
     devDependencies: [
-      "next",
-
+      /**
+       * React TS typings.
+       */
       "@types/react@^17.0.39",
       "@types/react-dom@^17.0.11",
-      "@tsmodule/react",
-
+      /**
+       * ESLint.
+       */
       "eslint",
       "eslint-config-next",
-
+      /**
+       * Build-time dependencies.
+       */
+      "@tsmodule/react",
+      "next",
       "tailwindcss",
       "autoprefixer",
       "cssnano",
--- src/specification/types.ts
+++ src/specification/types.ts
@@ -1,6 +1,12 @@
 export type ProjectType = "default" | "react";
 
+export type PackageJsonSettings = {
+  [key: string]: string | number | boolean | object;
+};
+
 export interface ProjectConfiguration {
+  files: string[];
+  packageJson: PackageJsonSettings;
   dependencies: string[];
   devDependencies: string[];
 }
