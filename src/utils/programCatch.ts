55e33037916871a4baaef7511beb91e1766171a8
C. Lewis <1657236+ctjlewis@users.noreply.github.com>
2022-08-11 12:47:01 -0500

    feat: upgrade user-facing logs (#27)

---
+++ src/utils/programCatch.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { bannerError } from "../commands";
+import chalk from "chalk";
+import { SpawnResult } from "await-shell";
+
+const isShellExit = (error: unknown): error is SpawnResult => {
+  const anyError = error as any;
+  if (
+    anyError.code === undefined ||
+    anyError.stdout == undefined ||
+    anyError.stderr == undefined
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+/* eslint-disable no-console */
+export const programCatch = (
+  fn:  (...args: any[]) => any | Promise<any>
+) => {
+  const wrapped = async (...args: any[]) => {
+    try {
+      return await fn(...args);
+    } catch (error) {
+      bannerError("Error");
+
+      if (isShellExit(error)) {
+        if (error.stdout) {
+          console.log(chalk.red(error.stdout));
+        }
+
+        if (error.stderr) {
+          console.log(chalk.redBright(error.stderr));
+        }
+      } else {
+        console.log(chalk.redBright(JSON.stringify(error, null, 2)));
+      }
+
+      console.log();
+    }
+  };
+
+  return wrapped;
+};
