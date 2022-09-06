db219e319dccf04bae9e13d48be23f998a222e1c
C. Lewis <ctj.lewis@icloud.com>
2022-09-06 04:46:42 -0500

    chore: execute with given Node

--- test/integration/commands.test.ts
+++ test/integration/commands.test.ts
@@ -42,7 +42,7 @@ test.before("[create] should create all template types", async () => {
     ...dirsToCopyDevInto,
   ]) {
     process.chdir(dirToLink);
-    await shell.run("npm link -f @tsmodule/tsmodule");
+    await shell.run("npm link -f --scripts-prepend-node-path @tsmodule/tsmodule");
   }
 });
 
