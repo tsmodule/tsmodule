e0d303a014f419a3c757b6f6a04dda83c75378c5
C. Lewis <ctj.lewis@icloud.com>
2023-02-18 06:39:52 -0600

    feat: add second tsconfig.tests.json TS program
    per @icehaunter
    https://github.com/tsmodule/tsmodule/issues/47

--- templates/default/test/example.test.ts
+++ templates/default/test/example.test.ts
@@ -1,7 +1,8 @@
-import test from "ava";
+/* eslint-disable no-constant-condition */
+import test from 'ava'
 
-test("two plus two should equal four", (t) => {
+test('two plus two should equal four', (t) => {
   if (2 + 2 === 4) {
-    t.pass();
+    t.pass()
   }
-});
+})
